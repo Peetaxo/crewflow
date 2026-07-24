ddf22f34808c1ccce905d0576b954011298049a1
Peetax <helpoff1@gmail.com>
2026-07-24 12:12:07 +0200

    chore: remove paid Google maps integration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_APP_DATA_SOURCE?: string;
   readonly VITE_SUPABASE_URL?: string;
   readonly VITE_SUPABASE_PUBLISHABLE_KEY?: string;
-  readonly VITE_GOOGLE_MAPS_API_KEY?: string;
 }
 
 interface ImportMeta {
